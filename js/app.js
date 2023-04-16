9b1e7090355d844a82b4dbc81297bf6eb029b139
Harsh Kumar <harshkumar742@gmail.com>
2023-04-16 22:42:08 +0530

    Change BaseURL

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const submitForm = document.getElementById('submit-application-form');
+const baseUrl = 'https://3e5e-2401-4900-1cb8-283e-e011-3312-8f6-6651.ngrok-free.app';
 
 submitForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -11,7 +12,6 @@ submitForm.addEventListener('submit', (e) => {
   const monthlyDebt = document.getElementById('monthly-debt').value;
   const employmentStatus = document.getElementById('employment-status').value;
   //const baseUrl = 'https://3.108.223.105:8000';
-  const baseUrl = 'https://3e5e-2401-4900-1cb8-283e-e011-3312-8f6-6651.ngrok-free.app'
 
   fetch(`${baseUrl}/api/loan_applications`, {
     method: 'POST',
